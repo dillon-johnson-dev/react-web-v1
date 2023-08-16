96b36fca55c5f9c29ac773f3077c21f0f26ce9cd
Dillon Johnson <dillonj323@gmail.com>
2023-08-17 01:28:15 +1200

    update about page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import ReactDom from 'react-dom'
+// import ReactDom from 'react-dom'
+// import App from './App'
+// import './index.css'
+
+// ReactDom.render(<App />, document.querySelector('#root'))
+
+import React from 'react'
+import { createRoot } from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDom.render(<App />, document.querySelector('#root'))
+const rootElement = document.querySelector('#root')
+
+const root = createRoot(rootElement)
+root.render(<App />)
